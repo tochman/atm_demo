9afde068043003ace13c529080485efb6a3070bf
tochman <thomas@craftacademy.se>
2016-06-13 15:19:57 +0200

    modifies withdraw method with condition to check account balance

--- lib/atm.rb
+++ lib/atm.rb
@@ -5,13 +5,22 @@ class ATM
     @funds = 1000
   end
 
-  def withdraw(amount)
-    deduct_funds_from_atm(amount)
+  def withdraw(amount, account)
+    case
+    when account_balance_to_low(amount, account) then
+      deduct_funds_from_atm(amount)
+    else
+      false
+    end
   end
 
   private
   def deduct_funds_from_atm(amount)
     @funds -= amount
+  end
+
+  def account_balance_to_low(amount, account)
+    amount <= account.balance
   end
 
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -2,13 +2,23 @@ require './lib/atm.rb'
 
 describe ATM do
 
+  let(:account) { double('Account', balance: 100) }
+
+  before do
+    allow(account).to receive(:balance=)
+  end
+
   it 'holds $1000 when created' do
     expect(subject.funds).to eq 1000
   end
 
   it 'deducts withdraw amount from ATM funds' do
-    subject.withdraw(100)
+    subject.withdraw(100, account)
     expect(subject.funds).to eq 900
+  end
+
+  it 'rejects withdraw if amount on account is to low' do
+    expect(subject.withdraw(150, account)).to eq false
   end
 
 end
